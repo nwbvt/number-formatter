fefb991bfc537fce6de7557916eaf8cba28fd0f3
Nick Brown <nwbrown@gmail.com>
2013-11-02 12:27:35 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to number-formatter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/number_formatter/core_test.clj
@@ -0,0 +1,7 @@
+(ns number-formatter.core-test
+  (:require [clojure.test :refer :all]
+            [number-formatter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
