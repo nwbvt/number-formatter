5f90a4b8d8b781a13ddf59615abc2dfe9c9c74ac
Nick Brown <nwbrown@gmail.com>
2013-11-02 13:26:14 -0400

    Stupid 15, ruining my plans

--- src/number_formatter/core.clj
+++ src/number_formatter/core.clj
@@ -1,35 +1,32 @@
 (ns number-formatter.core
   (:require [clojure.string :as string]))
 
-(defn- handle-constant
-  "Handle numbers small enough that they can't be broken down further"
-  [n]
-  (case n
-    1 "one"
-    2 "two"
-    3 "three"
-    4 "four"
-    5 "five"
-    6 "six"
-    7 "seven"
-    8 "eight"
-    9 "nine"
-    10 "ten"
-    11 "eleven"
-    12 "twelve"
-    13 "thirteen"))
+(def constants {1 "one"
+                2 "two"
+                3 "three"
+                4 "four"
+                5 "five"
+                6 "six"
+                7 "seven"
+                8 "eight"
+                9 "nine"
+                10 "ten"
+                11 "eleven"
+                12 "twelve"
+                13 "thirteen"
+                15 "fifteen"})
 
 (defn- handle-teen
   "Teens are a bit wierd"
   [n]
-  (str (handle-constant (- n 10)) "teen"))
+  (str (constants (- n 10)) "teen"))
 
 (defn num-format
   "Formats the number input into a string representation"
   [n]
   (string/capitalize
     (cond
-      (<= n 13) (handle-constant n)
+      (contains? constants n) (constants n)
       (<= n 19) (handle-teen n)
       :else n)))
 
--- test/number_formatter/core_test.clj
+++ test/number_formatter/core_test.clj
@@ -6,6 +6,8 @@
   (testing "Test that the formatter returns as expected"
     (is (= "One" (num-format 1)))
     (is (= "Eleven" (num-format 11)))
+    (is (= "Fourteen" (num-format 14)))
+    (is (= "Fifteen" (num-format 15)))
     (is (= "Sixteen" (num-format 16)))
 ;    (is (= "Two hundred twelve" (num-format 212)))
 ;    (is (= "Three thousand four hundred fifty six" (num-format 3456)))
