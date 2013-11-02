536d3137f1ad3a36c5b116266f986fec1954e2aa
Nick Brown <nwbrown@gmail.com>
2013-11-02 14:57:02 -0400

    Handle tens

--- src/number_formatter/core.clj
+++ src/number_formatter/core.clj
@@ -14,12 +14,27 @@
                 11 "eleven"
                 12 "twelve"
                 13 "thirteen"
-                15 "fifteen"})
+                15 "fifteen"
+                20 "twenty"
+                30 "thirty"
+                40 "fourty"
+                50 "fifty"
+                60 "sixty"
+                70 "seventy"
+                80 "eighty"
+                90 "ninety"})
 
 (defn- handle-teen
   "Teens are a bit wierd"
   [n]
   (str (constants (- n 10)) "teen"))
+
+(defn- handle-tens
+  "Handle numbers between 21 to 99"
+  [n]
+  (let [tens (* (int (/ n 10)) 10)
+        ones (mod n 10)]
+    (str (constants tens) "-" (constants ones))))
 
 (defn num-format
   "Formats the number input into a string representation"
@@ -27,7 +42,8 @@
   (string/capitalize
     (cond
       (contains? constants n) (constants n)
-      (<= n 19) (handle-teen n)
+      (< n 20) (handle-teen n)
+      (< n 100) (handle-tens n)
       :else n)))
 
 (defn -main
